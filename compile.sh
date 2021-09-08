824da7a7c22d7d3ac3d3856b5eaec63f10fba6e3
Yigong Hu <hyigong1@jhu.edu>
2021-09-08 19:22:00 -0400

    Upload compilation script

---
+++ compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p {dist,build}
+cd build
+
+../9.2.0/configure --prefix=$(pwd)/../dist
+make -j4
+if [ $? -ne 0 ]; then
+  echo "Failed to build memcached"
+  exit 1
+fi
+make install
+
