70f5399818782071cc6c7f6c34795d17cf87aab0
gongxini <hyigong1@jhu.edu>
2023-07-26 07:34:17 -0600

    update compile file

--- compile.sh
+++ compile.sh
@@ -2,7 +2,6 @@
 
 mkdir -p {dist,build}
 cd build
-sudo apt install -y libreadline-dev
 
 ../9.2.0/configure --prefix=$(pwd)/../dist
 make -j4
@@ -13,3 +12,7 @@ fi
 make install
 cd ../dist
 ./bin/initdb data
+cd ..
+echo "export PSANDBOX_POSTGRES_DIR=`pwd`/dist" >> $HOME/.bashrc
+echo "export LD_LIBRARY_PATH=`pwd`/dist/lib:$LD_LIBRARY_PATH" >> $HOME/.bashrc
+echo "export PATH=`pwd`/dist/bin:$PATH" >> $HOME/.bashrc
